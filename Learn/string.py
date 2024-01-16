78bc6fbe39aa769052d2a4d695c97e5ad8aabec8
Ath1thyan <athitasks@gmail.com>
2024-01-16 15:19:40 +0530

    string

---
+++ Learn/string.py
@@ -0,0 +1,10 @@
+name, age, gender = 'Athi', '23', 'Male'
+print(name, age, gender)
+
+like = dislike = 100
+print(like, dislike)
+
+num = 79162
+print("the num is " + str(num))
+num = "79162"
+print("the num is " + num)
