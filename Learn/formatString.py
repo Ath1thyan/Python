cc2055e457e8b428bb914aff7a0b6e62484f9f76
Ath1thyan <athitasks@gmail.com>
2024-03-06 16:35:31 +0530

    ...

--- Learn/formatString.py
+++ Learn/formatString.py
@@ -1,2 +1,6 @@
 name = "Athi"
 print(f"My name is {name} and length of the name is {len(name)}")
+print("Hello")  # By default print function has a newline character at the end.
+print("Hello", end="")  # To adding ( end="" ) to the end, newline will not be added.
+print("World")
+print("Hello", end="-->END\n") # To adding ( end="" ) in between words.
