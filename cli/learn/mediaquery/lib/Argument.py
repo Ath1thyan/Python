7eebb08b635274773d6d3252cdd38ded8103fd23
Ath1thyan <athitasks@gmail.com>
2024-06-16 09:33:05 +0530

    ...

---
+++ cli/learn/mediaquery/lib/Argument.py
@@ -0,0 +1,5 @@
+class Argument:
+    def __init__(self, args):
+        # sys.argv = args
+        self.args = args
+        print(self.args)
---
+++ cli/learn/mediaquery/mediaquery.py
@@ -0,0 +1,4 @@
+#! /Users/athis_mac/Developer/Labs/PlayGround/Python/bin/python
+import sys
+from lib.Argument import Argument
+a = Argument(sys.argv)
